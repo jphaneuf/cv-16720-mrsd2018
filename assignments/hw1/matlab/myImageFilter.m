4eed25f184eec3a60a85ad892882044f1b694ba3
jphaneuf <phaneuf.joe>
2018-01-28 17:23:58 -0500

    fix indexing bug in image filter. verify it does same thing if imfilter

--- assignments/hw1/matlab/myImageFilter.m
+++ assignments/hw1/matlab/myImageFilter.m
@@ -4,9 +4,7 @@
 %%%h: filter%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [img1] = myImageFilter(img0, h)
-  %Builtin padding ok?
-
+function [img_out] = myImageFilter(img0, h)
   %%Determine image and filter sizes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   [img_height img_width] = size(img0);
   [filter_height filter_width] = size(h);
@@ -14,7 +12,7 @@ function [img1] = myImageFilter(img0, h)
   pad_by = floor(filter_height/2);
 
   %%Init output image, pad input image %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  img_out = zeros(img_height,img_width)
+  img_out = zeros(img_height,img_width);
   img0 = padarray(img0,[pad_by pad_by],'symmetric','both');
 
   %%Vectorize filter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -24,9 +22,9 @@ function [img1] = myImageFilter(img0, h)
   vector_length = img_height * img_width;
   for i=0:vector_length-1;
     x = mod   (i , img_width  ) + 1;
-    y = floor (i / img_height ) + 1;
-    img_slice_vector = reshape(img0(x:x+2*pad_by,y:y+2*pad_by),1,[]);
-    img_out(y,x) = dot(img_slice_vector,h_vector)
+    y = floor (i / img_width ) + 1;
+    img_slice_vector = single(reshape(img0(y:y+2*pad_by,x:x+2*pad_by),1,[]));
+    img_out(y,x) = dot(img_slice_vector,h_vector);
   end
-  img_out
+  img_out;
 end
