54f1ba86e3b78e7b215fd2c5b4b08b2d4ca9fdd7
jphaneuf <phaneuf.joe>
2018-01-28 13:06:06 -0500

    sick packaging script

--- assignments/hw1/package.sh
+++ assignments/hw1/package.sh
@@ -1,2 +1,10 @@
-
-pandoc doc/jphaneuf.md -o jphaneuf.pdf
+#!/bin/sh
+OUTPUT=jphaneuf
+INPUT=$OUTPUT
+OUTPUTDIR=$OUTPUT
+rm -rf $OUTPUTDIR
+mkdir $OUTPUTDIR
+cp -r matlab $OUTPUTDIR
+pandoc doc/${INPUT}.md -o $OUTPUTDIR/${OUTPUT}.pdf
+zip -r ${OUTPUTDIR}.zip $OUTPUTDIR
+rm -rf $OUTPUTDIR
