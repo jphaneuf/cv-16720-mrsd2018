0e825a377cb60164fdc5604bf8d4a5a20f871734
jphaneuf <phaneuf.joe>
2018-01-29 13:00:39 -0500

    fix edge filter vector divide

--- assignments/hw1/matlab/myEdgeFilter.m
+++ assignments/hw1/matlab/myEdgeFilter.m
@@ -1,6 +1,28 @@
 function [Im Io Ix Iy] = myEdgeFilter(img, sigma)
-%Your implemention
 
+  %%Convert image format for processing and plotting%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+  img=im2double(img);
+
+  %%Create filters%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+  filter_size     = 2 * ceil(3*sigma) + 1;
+  gaussian_filter = fspecial('gaussian',filter_size,sigma);
+  sobel_filter_x  = fspecial('sobel');
+  sobel_filter_y  = sobel_filter_x';
+
+  %%Apply filters%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+  img_blurred = myImageFilter( img         , gaussian_filter );
+  Ix          = myImageFilter( img_blurred , sobel_filter_x  );
+  Iy          = myImageFilter( img_blurred , sobel_filter_y  );
+
+  %nms
+  %%Generate image magnitude%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+  Im = sqrt(Ix.^2 + Iy.^2);
+  Io = atan(Iy./Ix);
+  Im;
+  Io;
+  Ix;
+  Iy;
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 end
     
                 
