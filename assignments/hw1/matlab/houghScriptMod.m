7e108d01ca1c37001bb3348f1a9dc02c13293577
jphaneuf <phaneuf.joe>
2018-01-31 10:27:26 -0500

    houghscriptmod writes params

---
+++ assignments/hw1/matlab/houghScriptMod.m
@@ -0,0 +1,60 @@
+clear;
+
+datadir     = '../data';    %the directory containing the images
+%resultsdir  = '../results'; %the directory for dumping results
+resultsdir = strcat('../results_',datestr(now,'ddmmmmyyyy_HH_MM_SS'))
+param_file = strcat(resultsdir,'/params.txt')
+mkdir(resultsdir)
+
+%parameters
+sigma     = 2;
+threshold = 0.03;
+threshold = 0.5;
+rhoRes    = 2;
+thetaRes  = pi/90;
+thetaRes  = 1;
+nLines    = 50;
+%end of parameters
+
+fd = fopen(param_file,'w');
+fprintf(fd, 'sigma: %f \nthreshold: %f \nthreshold: %f \nrhoRes: %f \nthetaRes: %f \nthetaRes: %f\nnLines: %f', sigma, threshold, threshold, rhoRes, thetaRes, thetaRes, nLines)
+
+
+
+return
+imglist = dir(sprintf('%s/*.jpg', datadir));
+
+for i = 1:numel(imglist)
+    
+    %read in images%
+    [path, imgname, dummy] = fileparts(imglist(i).name);
+    img = imread(sprintf('%s/%s', datadir, imglist(i).name));
+    
+    if (ndims(img) == 3)
+        img = rgb2gray(img);
+    end
+    
+    img = double(img) / 255;
+   
+    %actual Hough line code function calls%  
+    [Im Io Ix Iy] = myEdgeFilter(img, sigma);   
+    [H,rhoScale,thetaScale] = myHoughTransform(Im, threshold, rhoRes, thetaRes);
+    [rhos, thetas] = myHoughLines(H, nLines);
+    lines = houghlines(Im>threshold, 180*(thetaScale/pi), rhoScale, [rhos,thetas],'FillGap',5,'MinLength',10);
+    
+    %everything below here just saves the outputs to files%
+    fname = sprintf('%s/%s_01edge.png', resultsdir, imgname);
+    imwrite(Im/max(Im(:)), fname);
+    fname = sprintf('%s/%s_02threshold.png', resultsdir, imgname);
+    imwrite(Im > threshold, fname);
+    fname = sprintf('%s/%s_03hough.png', resultsdir, imgname);
+    imwrite(H/max(H(:)), fname);
+    fname = sprintf('%s/%s_04lines.png', resultsdir, imgname);
+    
+    img2 = img;
+    for j=1:numel(lines)
+       img2 = drawLine(img2, lines(j).point1, lines(j).point2); 
+    end     
+    imwrite(img2, fname);
+end
+    
