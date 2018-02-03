84e3a8f3f5595af619f7ddffc38728889ceef4e0
jphaneuf <phaneuf.joe>
2018-02-03 15:33:13 -0500

    package uses latex. add results

--- assignments/hw1/package.sh
+++ assignments/hw1/package.sh
@@ -5,6 +5,7 @@ OUTPUTDIR=$OUTPUT
 rm -rf $OUTPUTDIR
 mkdir $OUTPUTDIR
 cp -r matlab $OUTPUTDIR
-pandoc doc/${INPUT}.md -o $OUTPUTDIR/${OUTPUT}.pdf
+#pandoc doc/${INPUT}.md -o $OUTPUTDIR/${OUTPUT}.pdf
+pdflatex doc/${INPUT}.tex -o $OUTPUTDIR/${OUTPUT}.pdf
 zip -r ${OUTPUTDIR}.zip $OUTPUTDIR
 rm -rf $OUTPUTDIR
