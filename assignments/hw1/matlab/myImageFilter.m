5b3304ea8d398972d2d53010bdbe0063c774bbfd
jphaneuf <phaneuf.joe>
2018-02-07 08:46:05 -0500

    clean imagefilter

--- assignments/hw1/matlab/myImageFilter.m
+++ assignments/hw1/matlab/myImageFilter.m
@@ -4,27 +4,34 @@
 %%%h: filter%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [img_out] = myImageFilter(img0, h)
+function [ img_out ] = myImageFilter( img0, h )
+  %% flip horizontally and vertically for convolution
+  %% TODO: switch filter/convolution from keyword argument
+  h = flipud( h );
+  h = fliplr( h );
+
   %%Determine image and filter sizes %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  [img_height img_width] = size(img0);
-  [filter_height filter_width] = size(h);
-  assert(filter_height == filter_width , "That filter is not symmetric bruh");
-  pad_by = floor(filter_height/2);
+  [ img_height img_width ]       = size( img0 );
+  [ filter_height filter_width ] = size( h );
+
+  assert( filter_height == filter_width , "That filter is not symmetric bruh" );
 
   %%Init output image, pad input image %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  img_out = zeros(img_height,img_width);
-  img0 = padarray(img0,[pad_by pad_by],'symmetric','both');
+  pad_by  = floor( filter_height/2 );
+  img_out = zeros( img_height , img_width );
+  img0    = padarray( img0 , [pad_by pad_by] , 'symmetric', 'both' );
 
   %%Vectorize filter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  h_vector = reshape(h,1,[]);
+  h_vector = reshape( h, 1 ,[] );
 
   %%Iterate on all pixels %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   vector_length = img_height * img_width;
   for i=0:vector_length-1;
-    x = mod   (i , img_width  ) + 1;
-    y = floor (i / img_width ) + 1;
-    img_slice_vector = single(reshape(img0(y:y+2*pad_by,x:x+2*pad_by),1,[]));
-    img_out(y,x) = dot(img_slice_vector,h_vector);
+    x                = mod    ( i , img_width ) + 1;
+    y                = floor  ( i / img_width ) + 1;
+
+    img_slice_matrix = img0( y : y + 2*pad_by , x : x + 2*pad_by );
+    img_slice_vector = single ( reshape( img_slice_matrix,1,[]));
+    img_out(y,x)     = dot    ( img_slice_vector,h_vector);
   end
-  img_out;
 end
