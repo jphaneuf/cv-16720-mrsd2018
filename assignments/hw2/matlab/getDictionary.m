ebb8f638a2f02ad1826e9b7b796205ed869c8c2e
jphaneuf <phaneuf.joe>
2018-02-16 17:57:04 -0500

    get dictionary calls harris and edge

--- assignments/hw2/matlab/getDictionary.m
+++ assignments/hw2/matlab/getDictionary.m
@@ -10,21 +10,33 @@ function [dictionary] = getDictionary(imgPaths, alpha, K, method)
 %                       represents a single visual word
     % -----fill in your implementation here --------
 
+   
+  filterBank = createFilterBank();
+  depth = length(filterBank) * 3; 
+  dictionary = zeros( 0 , depth );
+
   for i = 1 : length(imgPaths)
-    if i == 2
+    if i == 3 
       break
     end
     file_path = strcat( '../data/' , imgPaths ( i ) );
     img = imread( char ( file_path ) );
     img = rgb2gray ( img );
     corners = getHarrisPoints( img , alpha, 0.04 );
-    
+    img_filtered = extractFilterResponses(img, filterBank);
+    %for c in corners
+    for c = 1 : length( corners )
+      x = corners(c,1);
+      y = corners(c,2);
+      row = reshape(img_filtered(y,x,:) , 1 , [] )
+      dictionary = [ dictionary ; row ]
+    end
   end
 %  apply filterbank to img
-  dictionary = 0
   
     
 
     % ------------------------------------------
     
 end
+
