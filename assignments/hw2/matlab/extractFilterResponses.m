8d04fe2c5155e372ca914858a0416f8b11001bce
jphaneuf <phaneuf.joe>
2018-02-16 12:34:51 -0500

    extracted filter responses

--- assignments/hw2/matlab/extractFilterResponses.m
+++ assignments/hw2/matlab/extractFilterResponses.m
@@ -11,20 +11,27 @@ function [filterResponses] = extractFilterResponses(I, filterBank)
 
     %Convert input Image to Lab
     doubleI = double(I);
-    if length(size(doubleI)) == 2
+    if length(size(doubleI)) == 2;
         tmp = doubleI;
         doubleI(:,:,1) = tmp;
         doubleI(:,:,2) = tmp;
         doubleI(:,:,3) = tmp;
     end
-    [L,a,b] = RGB2Lab(doubleI(:,:,1), doubleI(:,:,2), doubleI(:,:,3));
+    %[L,a,b] = RGB2Lab(doubleI(:,:,1), doubleI(:,:,2), doubleI(:,:,3));
+    img = RGB2Lab(doubleI(:,:,1), doubleI(:,:,2), doubleI(:,:,3));
     h = size(I,1);
     w = size(I,2);
-
    
     % -----fill in your implementation here --------
+    n_filters       = length ( filterBank );
+    filterResponses = zeros  ( h , w , 3 * n_filters );
 
-    
+    for   f  = 1 : n_filters ;
+      for ch = 1 : 3;
+        imgf = imfilter ( img ( : , : ,ch )  , cell2mat ( filterBank ( f ) ) );
+        filterResponses ( : , : , 3 * ( f - 1 ) +  ch ) = imgf;
+      end
+    end
 
     % ------------------------------------------
 end
