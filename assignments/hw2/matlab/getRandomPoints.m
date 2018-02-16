9d70e7aa00eb89ffe5e1a18c61b99b94de8eb157
jphaneuf <phaneuf.joe>
2018-02-16 12:52:29 -0500

    get random

--- assignments/hw2/matlab/getRandomPoints.m
+++ assignments/hw2/matlab/getRandomPoints.m
@@ -4,13 +4,13 @@ function [points] = getRandomPoints(I, alpha)
 %   I:                      grayscale image
 %   alpha:                  random points
 % Output:
-%   points:                    point locations
+%   points:                    point locations ( x , y )
 %
 	% -----fill in your implementation here --------
-
-    
-
-    % ------------------------------------------
+  h              = size  ( I , 1 );
+  w              = size  ( I , 2 );
+  points         = rand  ( alpha , 2 );
+  points( : , 1) = round ( w * points ( : , 1 ) );
+  points( : , 2) = round ( h * points ( : , 2 ) );
 
 end
-
