f448850b53a8065b68e8ffcd3f32957b1e126044
jphaneuf <phaneuf.joe>
2018-02-18 10:59:25 -0500

    get image distance

--- assignments/hw2/matlab/getImageDistance.m
+++ assignments/hw2/matlab/getImageDistance.m
@@ -1,22 +1,22 @@
-function [dist] = getImageDistance(hist1, hist2, method)
-% Computes the distance from the feature vector (returned by getImageFeatures
-% or getImageFeaturesSPM) histogram to all of the feature vectors for the
-% training images.
-% Inputs:
-%   hist1:           image1 histogram
-%   hist2:           image2 histogram
-%   method:          string 'euclidean' or 'chi2'
+function [ dist ] = getImageDistance( hist1 , histSet , method )
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % Computes the distance from the feature vector (returned by getImageFeatures
+  % or getImageFeaturesSPM) histogram to all of the feature vectors for the
+  % training images.
+  % Inputs:
+  %   hist1:           image1 histogram
+  %   hist2:           image2 vector of histograms
+  %   method:          string 'euclidean' or 'chi2'
 
-% Outputs:
-%   dist:          distance between two histograms
+  % Outputs:
+  %   dist:          vector of distances between hist1 and histograms in histSet
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Note: Please update the function if you decide to implement the set version 
-% getImageDistance(hist1, histSet, method)
-
-	% -----fill in your implementation here --------
-
-    
-
-    % ------------------------------------------
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  if     method == 'euclidean'
+    dist = pdist2 ( hist1 , histSet , 'euclidean' )
+  elseif method == 'chi2'
+    dist = pdist2 ( hist1 , histSet , 'chisq' )
+  end
 
 end
