d0a261c8c99a10440d230b1243065080a3e8488c
jphaneuf <phaneuf.joe>
2018-02-21 15:19:22 -0500

    bug fixes

--- assignments/hw2/matlab/batchToVisualWords.m
+++ assignments/hw2/matlab/batchToVisualWords.m
@@ -1,76 +1,77 @@
-function batchToVisualWords(numCores) 
+%function batchToVisualWords(numCores,dictionary_name) 
+function batchToVisualWords(numCores)
 
-% Does parallel computation of the visual words 
-%
-% Input:
-%   numCores - number of cores to use (default 2)
+  % Does parallel computation of the visual words 
+  %
+  % Input:
+  %   numCores - number of cores to use (default 2)
 
-if nargin < 1
-    %default to 2 cores
-    numCores = 2;
-end
+  if nargin < 1
+      %default to 2 cores
+      numCores = 2;
+  end
 
-% Close the pools, if any
-try
-    fprintf('Closing any pools...\n');
-%     matlabpool close; 
-    delete(gcp('nocreate'))
-catch ME
-    disp(ME.message);
-end
+  % Close the pools, if any
+  try
+      fprintf('Closing any pools...\n');
+  %     matlabpool close; 
+      delete(gcp('nocreate'))
+  catch ME
+      disp(ME.message);
+  end
 
-fprintf('Starting a pool of workers with %d cores\n', numCores);
-% matlabpool('local',numCores);
-parpool('local', numCores);
+  fprintf('Starting a pool of workers with %d cores\n', numCores);
+  % matlabpool('local',numCores);
+  parpool('local', numCores);
 
-%load the files and texton dictionary
-load('../data/traintest.mat','all_imagenames','mapping');
-load('dictionaryHarris.mat');
+  %load the files and texton dictionary
+  load('../data/traintest.mat','all_imagenames','mapping');
+  load ( dictionary_name );
+  %load('dictionaryHarris.mat');
+  %uncomment the following line to run the random version
+  % load('dictionaryRandom.mat');
 
-%uncomment the following line to run the random version
-% load('dictionaryRandom.mat');
+  filterBank = createFilterBank()
 
-filterBank = createFilterBank()
+  source = '../data/';
+  target = '../data/';
 
-source = '../data/';
-target = '../data/';
+  if ~exist(target, 'dir')
+      mkdir(target);
+  end
 
-if ~exist(target, 'dir')
-    mkdir(target);
-end
+  for category = mapping
+      if ~exist([target,category{1}],'dir')
+          mkdir([target,category{1}]);
+      end
+  end
 
-for category = mapping
-    if ~exist([target,category{1}],'dir')
-        mkdir([target,category{1}]);
-    end
-end
+  %This is a peculiarity of loading inside of a function with parfor. We need to 
+  %tell MATLAB that these variables exist and should be passed to worker pools.
+  filterBank = filterBank;
+  dictionary = dictionary;
 
-%This is a peculiarity of loading inside of a function with parfor. We need to 
-%tell MATLAB that these variables exist and should be passed to worker pools.
-filterBank = filterBank;
-dictionary = dictionary;
+  %matlab can't save/load inside parfor; accumulate
+  %them and then do batch save
+  l = length(all_imagenames);
 
-%matlab can't save/load inside parfor; accumulate
-%them and then do batch save
-l = length(all_imagenames);
+  wordRepresentation = cell(l,1);
+  parfor i=1:l
+      fprintf('Converting to visual words %s\n', all_imagenames{i});
+      image = imread([source, all_imagenames{i}]);
+      wordRepresentation{i} = getVisualWords(image, dictionary, filterBank);
+  end
 
-wordRepresentation = cell(l,1);
-parfor i=1:l
-    fprintf('Converting to visual words %s\n', all_imagenames{i});
-    image = imread([source, all_imagenames{i}]);
-    wordRepresentation{i} = getVisualWords(image, dictionary, filterBank);
-end
+  %dump the files
+  fprintf('Dumping the files\n');
+  for i=1:l
+      wordMap = wordRepresentation{i};
+      save([target, strrep(all_imagenames{i},'.jpg','.mat')],'wordMap');
+  end
 
-%dump the files
-fprintf('Dumping the files\n');
-for i=1:l
-    wordMap = wordRepresentation{i};
-    save([target, strrep(all_imagenames{i},'.jpg','.mat')],'wordMap');
-end
-
-%close the pool
-fprintf('Closing the pool\n');
-%matlabpool close
+  %close the pool
+  fprintf('Closing the pool\n');
+  %matlabpool close
 
 end
 
