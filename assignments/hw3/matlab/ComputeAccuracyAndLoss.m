9330cfb5d242b44722a3af98f6321d9df1fa2c95
jphaneuf <phaneuf.joe>
2018-03-04 17:35:36 -0500

    refactor init function. comput accuracy and loss

--- assignments/hw3/matlab/ComputeAccuracyAndLoss.m
+++ assignments/hw3/matlab/ComputeAccuracyAndLoss.m
@@ -1,7 +1,36 @@
 function [accuracy, loss] = ComputeAccuracyAndLoss(W, b, data, labels)
-% [accuracy, loss] = ComputeAccuracyAndLoss(W, b, X, Y) computes the networks
-% classification accuracy and cross entropy loss with respect to the data samples
-% and ground truth labels provided in 'data' and labels'. The function should return
-% the overall accuracy and the average cross-entropy loss.
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % [accuracy, loss] = ComputeAccuracyAndLoss(W, b, X, Y) computes the networks
+  % classification accuracy and cross entropy loss with respect to the data samples
+  % and ground truth labels provided in 'data' and labels'. The function should return
+  % the overall accuracy and the average cross-entropy loss.
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+  [ n_points input_size ] = size ( data );
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% compute predictions , extract predictions as maximum probability %%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  classifications     = Classify ( W , b , data );
+  [ val predictions ] = max ( classifications , [ ] ,2 );
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% Label vectors are one hot, so zeros cancel except for actual label %%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % labels should be one hot. 
+  loss =  - log ( classifications .* labels ) ;
+  loss = mean ( loss ( ~isinf ( loss ) ) );
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% Get actual label at predicted ( 1 if success , 0 if not ) %%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  [ x labels ] = ind2sub ( size ( labels ) , find ( labels ) );
+  accuracy = sum ( labels == predictions ) / n_points;
+
+end
+
+
+function [ cross_entropy_loss_vector ] = cross_entropy_loss ( label_vector , prediction_vector )
+  [ val prediction ] = max ( label_vector )
 end
--- assignments/hw3/matlab/InitializeNetwork.m
+++ assignments/hw3/matlab/InitializeNetwork.m
@@ -1,26 +1,24 @@
 function [W, b] = InitializeNetwork(layers)
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % InitializeNetwork([INPUT, HIDDEN, OUTPUT]) initializes the weights and biases
   % for a fully connected neural network with input data size INPUT, output data
   % size OUTPUT, and HIDDEN number of hidden units.
   % It should return the cell arrays 'W' and 'b' which contain the randomly
   % initialized weights and biases for this neural network.
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   n_entries = length ( layers );
-  W         = cell(n_entries,1);
-  b         = cell(n_entries,1);
-  INPUT     = layers ( 1 ) ;
-  HIDDEN    = layers ( 2 : length ( layers ) - 1 );
-  OUTPUT    = layers  ( length ( layers ) ) ;
+  W         = cell   ( 1 , n_entries - 1);
+  b         = cell   ( 1 , n_entries - 1 );
 
-  W { 1 }         = rand ( INPUT  , 1 )
-  W { n_entries } = rand ( OUTPUT , 1 )
-  b { 1 }         = rand 
-  b { n_entries } = rand
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% Populate layer weights and biases %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  for i = 1 : n_entries -1
+    i
+    W { i }         = rand ( layers ( i + 1 ) , layers ( i ) );
+    b { i }         = rand ( layers ( i + 1 ) , 1 );
 
-  index = 2
-  for  n = HIDDEN
-    W { index } = rand ( n  , 1 )
-    b { index } = rand 
-    index       = index + 1
-  end
-
+  end 
 end
