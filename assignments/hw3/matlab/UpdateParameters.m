9029480720dfae7329ec746c11006ea4865b29e0
jphaneuf <phaneuf.joe>
2018-03-06 16:45:25 -0500

    suppress rogue prints

--- assignments/hw3/matlab/UpdateParameters.m
+++ assignments/hw3/matlab/UpdateParameters.m
@@ -2,11 +2,11 @@ function [W, b] = UpdateParameters(W, b, grad_W, grad_b, learning_rate)
 % [W, b] = UpdateParameters(W, b, grad_W, grad_b, learning_rate) computes and returns the
 % new network parameters 'W' and 'b' with respect to the old parameters, the
 % gradient updates 'grad_W' and 'grad_b', and the learning rate.
-  [ x n_layers ] = size ( W )
+  [ x n_layers ] = size ( W );
   
   for i = 1 : n_layers
-    W{i} = W{i} - learning_rate * grad_W{i}  
-    b{i} = b{i} - learning_rate * grad_b{i}  
+    W{i} = W{i} - learning_rate * grad_W{i};
+    b{i} = b{i} - learning_rate * grad_b{i};
   end
 
 end
