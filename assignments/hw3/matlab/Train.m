8de298a7b995e5f1b22955534593714a14d189b3
jphaneuf <phaneuf.joe>
2018-03-06 14:06:43 -0500

    train first pass

--- assignments/hw3/matlab/Train.m
+++ assignments/hw3/matlab/Train.m
@@ -1,24 +1,25 @@
 function [W, b] = Train(W, b, train_data, train_label, learning_rate)
-% [W, b] = Train(W, b, train_data, train_label, learning_rate) trains the network
-% for one epoch on the input training data 'train_data' and 'train_label'. This
-% function should returned the updated network parameters 'W' and 'b' after
-% performing backprop on every data sample.
+  % [W, b] = Train(W, b, train_data, train_label, learning_rate) trains the network
+  % for one epoch on the input training data 'train_data' and 'train_label'. This
+  % function should returned the updated network parameters 'W' and 'b' after
+  % performing backprop on every data sample.
 
 
-% This loop template simply prints the loop status in a non-verbose way.
-% Feel free to use it or discard it
+  % This loop template simply prints the loop status in a non-verbose way.
+  % Feel free to use it or discard it
 
 
-for i = 1:size(train_data,1)
-
-
-
+  % We overwrite W and b , yes? or is there some batch trickery?
+  for i = 1:size(train_data,1)
+    X = train_data  ( i , : );
+    Y = train_label ( i , : ) ;
+    [ out, act_h, act_a ] = Forward  ( W, b, X );
+    [ grad_W , grad_b ]   = Backward ( W, b, X, Y, act_h, act_a)
+    [W, b] = UpdateParameters(W, b, grad_W, grad_b, learning_rate)
     if mod(i, 100) == 0
         fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b')
         fprintf('Done %.2f %%', i/size(train_data,1)*100)
     end
-end
-fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b')
-
-
+  end
+  fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b')
 end
