f9cc502ca309ff675236e8d15d303a050358be04
jphaneuf <phaneuf.joe>
2018-03-06 23:01:46 -0500

    dsigma element wise

--- assignments/hw3/matlab/dsigma.m
+++ assignments/hw3/matlab/dsigma.m
@@ -1,5 +1,5 @@
 function [ dsigma_out ] = dsigma ( x )
-  dsigma_out = sigmoid ( x ) * ( 1 - sigmoid ( x ) );
+  dsigma_out = sigmoid ( x ) .* ( 1 - sigmoid ( x ) );
 end
 
 function [ x ] = sigmoid ( x )
