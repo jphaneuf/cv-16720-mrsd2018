36e75082a7c283121c9decba8ccfe81b78fc42bf
jphaneuf <phaneuf.joe>
2018-03-07 13:10:11 -0500

    extract images pads and predicts

--- assignments/hw3/matlab/extractImageText.m
+++ assignments/hw3/matlab/extractImageText.m
@@ -3,17 +3,57 @@ function [text] = extractImageText(fname)
   % and returns the next contained in the image as a string.
   img = imread ( fname );
   [lines, bw] = findLetters( img );
-  hold off;
-  imshow ( bw );
-  hold on;
+  %hold off;
+  %imshow ( bw );
+  %hold on;
+   
+  load('nist26_model');
+  characters = {};
   for line = 1: length ( lines ) %horizontal line
     %for letter = 1 : length ( line )
     letter_boxes = lines{line}{1};
     [ n_letters x ] = size (letter_boxes);
-    for b = 1 : n_letters
-      box = lines {line}{1}(b,:);
-      boxx = [box(1) , box(2) , (box(3)-box(1)) , (box(4) - box(2)) ];
-      rectangle('pos' , boxx );
+    for bi = 1 : n_letters
+      % How did we get non-ints here???
+      box = floor (lines {line}{1}(bi,:) );
+      if all ( box > 0 )
+        ch = bw ( box(2):box(4) , box(1):box(3) );
+        ch = resize_and_pad ( ch );
+        characters{end+1} = ch;
+        X = reshape ( ch , [] , 1 );
+        [out, act_h, act_a] = Forward(W, b, X);
+        [val label] = max ( out );
+        label
+        fprintf('detected character %s', char ( 64 + label ) );
+        char ( 65 + label );
+        
+        imshow ( ch , []);
+        pause ( 2 )
+        %%% rectangle form for plotting bounding boxes : [x y w h]:
+        %boxx = [box(1) , box(2) , (box(3)-box(1)) , (box(4) - box(2)) ];
+        %rectangle('pos' , boxx );
+      end
     end
   end
+end
+
+
+function [ imgout ] = resize_and_pad ( img )
+  [ rows cols ] = size ( img );
+  OUTSIZE = 32;
+  imgout = ones ( OUTSIZE , OUTSIZE );% * 255;
+  if rows > cols
+    scale = OUTSIZE / rows;
+    img = imresize ( img , scale );
+    [ rows cols ] = size ( img );
+    col_index = (OUTSIZE/2) - floor(cols / 2);
+    imgout ( : , col_index + 1: col_index + cols ) = img;
+  else
+    scale = OUTSIZE / cols;
+    img = imresize ( img , scale );
+    [ rows cols ] = size ( img );
+    row_index = (OUTSIZE/2) - floor(rows / 2);
+    imgout ( row_index + 1: row_index + rows , : ) = img;
+  end
+  
 end
