692753455b218c80afb1b175f1111f872848af96
jphaneuf <phaneuf.joe>
2018-03-07 07:45:48 -0500

    findLetters. bounding box with widht

--- assignments/hw3/matlab/findLetters.m
+++ assignments/hw3/matlab/findLetters.m
@@ -1,4 +1,7 @@
 function [lines, bw] = findLetters( img )
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % [lines, BW] = findLetters(im) processes the input RGB image and returns a cell
   % array 'lines' of located characters in the image, as well as a binary
   % representation of the input image. The cell array 'lines' should contain one
@@ -7,21 +10,45 @@ function [lines, bw] = findLetters( img )
   % Each row of the matrix should contain 4 numbers [x1, y1, x2, y2] representing
   % the top-left and bottom-right position of each box. The boxes in one line should
   % be sorted by x1 value.
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  [ rows cols ] = size ( img ) ;
+  DF_char = strel('rectangle',[ 30 , 20 ]);
+  DF_line = strel('rectangle',[ 40 , 300 ]);
 
+  bw       = im2bw ( img  );
+  img      = im2bw ( imcomplement ( img ) );
+  img_char = img;
 
-  DF = strel('rectangle',[ 30 , 20 ]);
-
-  %img = imread('../images/01_list.jpg');
-  img = im2bw ( imcomplement ( img ) );
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% Extract character position %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
   for i = 1:5
-    img = imdilate ( img , DF );
-    img = imerode ( img , DF ) ;
+    img_char = imdilate ( img_char , DF_char );
+    img_char = imerode  ( img_char , DF_char );
   end
 
-  imshow ( img ) ; hold on;
-  CC = bwconncomp(img)
-  S  = regionprops(CC,'Centroid')
+  imshow ( img_char ) ; hold on;
+  CC = bwconncomp(img_char);
+  S  = regionprops(CC,'Centroid');
+  Z = regionprops ( CC , 'BoundingBox' ) ;
+  bounding_boxes = reshape ( [Z.BoundingBox] , 4 , [] )';
   centroids = reshape ( [S.Centroid] , 2 , [] )';
   scatter ( centroids ( : , 1 ) , centroids ( : , 2 ) , 'rx' );
+  cleval = evalclusters(bounding_boxes ( : , 2 ) ,'gmdistribution','CalinskiHarabasz','KList',[1:30]) ;
+  [ ks ys ] = kmeans ( bounding_boxes ( : , 2 ) , cleval.OptimalK );
+  for i = 1 : cleval.OptimalK
+    x = [ 1 , cols ];
+    plot ( x , ones ( 1 , 2 ) * ys ( i ) );
+  end
+  lines = cell ( cleval.OptimalK , 1)
+  for i = 1 : cleval.OptimalK
+    li = 1
+    for l = find ( ks == i ) %real indices
+      letters{li} = bounding_boxes ( l , : );
+      li = li+1
+    end
+    lines{i} = letters
+  end
 end
