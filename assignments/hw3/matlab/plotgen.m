6787cdce36dff3423223362b06ad8611365b1416
jphaneuf <phaneuf.joe>
2018-03-08 07:49:37 -0500

    plotgen gotta have a monttaaaaaggge

--- assignments/hw3/matlab/plotgen.m
+++ assignments/hw3/matlab/plotgen.m
@@ -1,6 +1,10 @@
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% accuracy and loss plots %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%{
 % train_acc train_loss valid_acc valid_loss
-load('statsq311')
+load('statsq313')
 [ n_epochs , nentries ] = size ( stats )
 
 subplot ( 1 , 2 , 1 );
@@ -11,7 +15,7 @@ xlabel('n epochs');
 ylabel('accuracy');
 plot ( 1:n_epochs , stats ( : , 3 ) )
 legend ( 'training accuracy','validation accuracy' )
-title('training accuracy and loss');
+title('training accuracy and loss learning 0.001');
 
 
 subplot ( 1 , 2 , 2 );
@@ -23,3 +27,21 @@ ylabel('cross entropy loss');
 plot ( 1:n_epochs , stats ( : , 4 ) )
 legend ( 'training loss','validation loss' )
 
+%}
+
+
+%% You gotta have a montage %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('nist26_model');
+[ nW INPUT ]  = size ( W{1} );
+
+%Want [ 32 32 3 400 ]
+W1 = double ( W{1} );
+W1m = zeros ( [ 32 32 3 400] );
+for i = 1 : nW
+  img = W1 ( i , : );
+  img = reshape ( img , [ 32 32 ] );
+  img = cat ( 3 , img , img , img );
+  W1m ( : , : , : , i ) = img ;
+end
+
+montage ( W1m )
