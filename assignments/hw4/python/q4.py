6ec60b4cac0a4847b34cff26f3fdd175d7eac5df
jphaneuf <phaneuf.joe>
2018-03-25 23:59:12 -0400

    panorama ding dong

--- assignments/hw4/python/q4.py
+++ assignments/hw4/python/q4.py
@@ -2,6 +2,7 @@ import numpy as np
 from q2 import *
 from q3 import *
 import skimage.color
+from scipy import misc
 
 # you may find this useful in removing borders
 # from pnc series images (which are RGBA)
@@ -19,49 +20,56 @@ def clip_alpha(img):
 # feel free to hardcode output size
 def imageStitching(img1, img2, H2to1):
   #warps img2 into img1 frame
-  out_shape = [ 600 , 600 , 3 ]
-  panoImg = np.zeros ( out_shape ) #img1
-  ##############################################################################
-  ## Will predict total image size now for use in warp, rather than ############
-  #### mess with it later#######################################################
-  ##############################################################################
-  h1 , w1 , x = img1.shape
-  h2 , w2 , x = img2.shape
-  ##########################    x    y    1  ###################################
-  img2_corners = np.array ( [ [ 0  , 0  , 1 ] , 
-                              [ w2 , 0  , 1 ] , 
-                              [ 0  , h2 , 1 ] ,
-                              [ w2 , h2 , 1 ] ] ).T
+  out_shape = [ 315 , 310 , 3 ]
+  panoImg   = np.zeros ( out_shape )
 
-  img1_corners = np.array ( [ [ 0  , 0  , 1 ] , 
-                              [ w1 , 0  , 1 ] , 
-                              [ 0  , h1 , 1 ] ,
-                              [ w1 , h1 , 1 ] ] ).T
-
-  img2_corners_warped = np.dot ( H2to1 , img2_corners )
-  ## Normalize #################################################################
-  #img2_corners_warped [ 0 , : ] = np.divide ( img2_corners_warped [ 0 , : ] , 
-  #                                            img2_corners_warped [ 2 , : ] )
-  #img2_corners_warped [ 1 , : ] = np.divide ( img2_corners_warped [ 0 , : ] , 
-  #                                            img2_corners_warped [ 2 , : ] )
-
-  #corners = np.hstack ( ( img1_corners , img2_corners_warped ) )
-  
-
-  ##############################################################################
-  warped       = warp ( img2 , H2to1  , output_shape = [ 600 , 600 ] , 
+  img2_warped       = warp ( img2 , H2to1  , output_shape = out_shape , 
+                        mode = 'constant' , cval = 0 )
+  img1_warped       = warp ( img1 , np.eye ( 3 ) , output_shape = out_shape ,  
                         mode = 'constant' , cval = 0 )
 
   ##############################################################################
   ## Extract indices of non-zero Harry pixels , set them in composite ##########
   ##############################################################################
-  warped = warped [ : , : , 0:3 ]
-  indices      = warped       [ : , : , 0   ] != 0
-  panoImg = panoImg [ : , : , 0:3 ] # what is this 4th thing?
-  img1 = img1 [ : , : , 0:3 ] #..?
-  panoImg [ indices ] = warped [ indices ]
+  img1_warped = img1_warped [ : , : , 0:3 ]
+  img2_warped = img2_warped [ : , : , 0:3 ]
+  indices1      = img1_warped       [ : , : , 0   ] != 0
+  indices2      = img2_warped       [ : , : , 0   ] != 0
+  #panoImg = panoImg [ : , : , 0:3 ] # what is this 4th thing?
+  panoImg [ indices2 ] = img2_warped [ indices2 ]
+  panoImg [ indices1 ] = img1_warped [ indices1 ]
+  #panoImg =  misc.imfilter ( panoImg , ftype = 'blur' )
   
   return panoImg
+
+##############################################################################
+## Will predict total image size now for use in warp, rather than ############
+#### mess with it later#######################################################
+##############################################################################
+#h1 , w1 , x = img1.shape
+#h2 , w2 , x = img2.shape
+##########################    x    y    1  ###################################
+"""
+img2_corners = np.array ( [ [ 0  , 0  , 1 ] , 
+                            [ w2 , 0  , 1 ] , 
+                            [ 0  , h2 , 1 ] ,
+                            [ w2 , h2 , 1 ] ] ).T
+
+img1_corners = np.array ( [ [ 0  , 0  , 1 ] , 
+                            [ w1 , 0  , 1 ] , 
+                            [ 0  , h1 , 1 ] ,
+                            [ w1 , h1 , 1 ] ] ).T
+
+"""
+#img2_corners_warped = np.dot ( H2to1 , img2_corners )
+## Normalize #################################################################
+#img2_corners_warped [ 0 , : ] = np.divide ( img2_corners_warped [ 0 , : ] , 
+#                                            img2_corners_warped [ 2 , : ] )
+#img2_corners_warped [ 1 , : ] = np.divide ( img2_corners_warped [ 0 , : ] , 
+#                                            img2_corners_warped [ 2 , : ] )
+
+#corners = np.hstack ( ( img1_corners , img2_corners_warped ) )
+
 
 
 # Q 4.2
