86e36b1e5eb7af81da5db4e956cdbc2d086aa336
jphaneuf <phaneuf.joe>
2018-03-26 09:46:29 -0400

    q4.2

--- assignments/hw4/python/q4.py
+++ assignments/hw4/python/q4.py
@@ -2,6 +2,7 @@ import numpy as np
 from q2 import *
 from q3 import *
 import skimage.color
+import skimage.transform
 from scipy import misc
 
 # you may find this useful in removing borders
@@ -42,44 +43,81 @@ def imageStitching(img1, img2, H2to1):
   
   return panoImg
 
-##############################################################################
-## Will predict total image size now for use in warp, rather than ############
-#### mess with it later#######################################################
-##############################################################################
-#h1 , w1 , x = img1.shape
-#h2 , w2 , x = img2.shape
-##########################    x    y    1  ###################################
-"""
-img2_corners = np.array ( [ [ 0  , 0  , 1 ] , 
-                            [ w2 , 0  , 1 ] , 
-                            [ 0  , h2 , 1 ] ,
-                            [ w2 , h2 , 1 ] ] ).T
-
-img1_corners = np.array ( [ [ 0  , 0  , 1 ] , 
-                            [ w1 , 0  , 1 ] , 
-                            [ 0  , h1 , 1 ] ,
-                            [ w1 , h1 , 1 ] ] ).T
-
-"""
-#img2_corners_warped = np.dot ( H2to1 , img2_corners )
-## Normalize #################################################################
-#img2_corners_warped [ 0 , : ] = np.divide ( img2_corners_warped [ 0 , : ] , 
-#                                            img2_corners_warped [ 2 , : ] )
-#img2_corners_warped [ 1 , : ] = np.divide ( img2_corners_warped [ 0 , : ] , 
-#                                            img2_corners_warped [ 2 , : ] )
-
-#corners = np.hstack ( ( img1_corners , img2_corners_warped ) )
-
 
 
 # Q 4.2
 # you should make the whole image fit in that width
 # python may be inv(T) compared to MATLAB
 def imageStitching_noClip(img1, img2, H2to1, panoWidth=1280):
-    panoImg = None
-    # YOUR CODE HERE
-    
-    return panoImg
+  M = np.eye ( 3 )
+  # Assume img2 to the right, otherwise need some extra processing
+  ##############################################################################
+  ## Will predict total image size now for use in warp #########################
+  ##############################################################################
+  h1 , w1 , x = img1.shape
+  h2 , w2 , x = img2.shape
+  ##########################    x    y    1  ###################################
+  img2_corners = np.array ( [ [ 0  , 0  , 1 ] , 
+                              [ w2 , 0  , 1 ] , 
+                              [ 0  , h2 , 1 ] ,
+                              [ w2 , h2 , 1 ] ] ).T
+
+  img1_corners = np.array ( [ [ 0  , 0  , 1 ] , 
+                              [ w1 , 0  , 1 ] , 
+                              [ 0  , h1 , 1 ] ,
+                              [ w1 , h1 , 1 ] ] ).T
+
+  img2_corners_warped = np.dot ( np.linalg.inv ( H2to1 ) , img2_corners )
+  img2_corners_warped = normPoints ( img2_corners_warped )
+
+  corners = np.hstack ( ( img1_corners , img2_corners_warped ) )
+  xmin = min ( corners [ 0 , : ] )
+  xmax = max ( corners [ 0 , : ] )
+  ymin = min ( corners [ 1 , : ] )
+  ymax = max ( corners [ 1 , : ] )
+
+  out_shape = np.array ( [ ymax - ymin , xmax - xmin , 3 ] )
+  out_shape = out_shape.astype ( 'int' )
+
+
+  T = np.eye ( 3 )
+  T [ 0 , 2 ] = xmin 
+  T [ 1 , 2 ] = ymin
+  S = np.eye ( 3 )
+  ### Scaling this way is janky , resize so everything fits in frame 1
+  ### with translations, then resize so width = panoWidth
+  #scale_factor = out_shape [ 1 ] / float ( panoWidth )
+  #scale_factor = float ( panoWidth ) / out_shape [ 1 ]
+  #out_shape [ 0 : 2 ] = scale_factor * out_shape [ 0 : 2 ] 
+  #out_shape [ 0 : 2 ] = panoWidth * out_shape [ 0 : 2 ] / out_shape [ 1 ]
+  #S [ 0 , 0 ] = 1 / float ( scale_factor )
+  #S [ 1 , 1 ] = 1 / float ( scale_factor )
+  #S [ 0 , 0 ] = scale_factor
+  #S [ 1 , 1 ] = scale_factor
+  M = np.dot ( S , T )
+
+  panoImg   = np.zeros ( out_shape )
+
+  img2_warped       = warp ( img2 , np.dot ( M , H2to1 ) , output_shape = out_shape , 
+                        mode = 'constant' , cval = 0 )
+  img1_warped       = warp ( img1 , M , output_shape = out_shape ,  
+                        mode = 'constant' , cval = 0 )
+
+  ##############################################################################
+  ## Extract indices of non-zero Harry pixels , set them in composite ##########
+  ##############################################################################
+  img1_warped = img1_warped [ : , : , 0:3 ]
+  img2_warped = img2_warped [ : , : , 0:3 ]
+  indices1      = img1_warped        [ : , : , 0   ] != 0
+  indices2      = img2_warped        [ : , : , 0   ] != 0
+  panoImg [ indices2 ] = img2_warped [ indices2 ]
+  panoImg [ indices1 ] = img1_warped [ indices1 ]
+  #panoImg =  misc.imfilter ( panoImg , ftype = 'blur' )
+  
+  out_shape [ 0 : 2 ] = panoWidth * out_shape [ 0 : 2 ] / out_shape [ 1 ]
+  panoImg = skimage.transform.resize ( 
+             panoImg , out_shape )
+  return panoImg
 
 # Q 4.3
 # should return a stitched image
