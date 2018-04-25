9e6ba3dc197aa36313332d4b540a9bd7b9d182dd
jphaneuf <phaneuf.joe>
2018-04-25 19:45:38 -0400

    test carzzz

--- assignments/hw6/matlab/testCarSequence.m
+++ assignments/hw6/matlab/testCarSequence.m
@@ -5,7 +5,6 @@ function  [ ] = testCarSequence( )
   load ( '../data/carseq.mat' )   ;
   n_frames = size ( frames , 3 )  ;
   rect = [ 60 ; 117 ; 146 ; 152 ]; % Init bounding box
-  %for i = 1 : n_frames - 1
   for i = 1 : n_frames - 1
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     imshow ( frames ( : , : , i ) );
@@ -14,15 +13,14 @@ function  [ ] = testCarSequence( )
     w = rect ( 3 ) - rect ( 1 );
     h = rect ( 4 ) - rect ( 2 );
     %rectangle ( 'Position' , rect' );
-    fuck_rect = [ x , y , w , h ]
+    fuck_rect = [ x , y , w , h ];
     rectangle ( 'Position' , [ x , y , w , h ]  );
     [ u, v ] = LucasKanadeInverseCompositional( frames ( : , : , i     ) ,  ...
                                                 frames ( : , : , i + 1 ) ,  ...
-                                                rect                       );
-    fuck_rect
+                                                rect                       )
     rect
     rect = rect + [ u ; v ; u ; v ]
-    pause (0.1 );
+    pause ( 0.01 )
   end
   %[u,v] = LucasKanadeInverseCompositional(It, It1, rect)
 end
