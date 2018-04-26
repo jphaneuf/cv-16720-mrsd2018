db975ba768b129a4882b590ff18792fe81d76342
jphaneuf <phaneuf.joe>
2018-04-25 21:30:40 -0400

    basis is working

--- assignments/hw6/matlab/LucasKanadeBasis.m
+++ assignments/hw6/matlab/LucasKanadeBasis.m
@@ -3,3 +3,65 @@ function [u,v] = LucasKanadeBasis(It, It1, rect, bases)
 % input - image at time t, image at t+1, rectangle (top left, bot right
 % coordinates), bases 
 % output - movement vector, [u,v] in the x- and y-directions.
+  %Precompute
+  THRESHOLD = 0.00001;
+  It = double ( It );
+  It1 = double ( It1 );
+  [ h , w ] = size ( It );
+  max_iters = 1000; 
+  rect = round ( rect , 6 ); %....really?
+  x1 = rect ( 1 );
+  y1 = rect ( 2 );
+  x2 = rect ( 3 );
+  y2 = rect ( 4 );
+
+  T  = interp2 ( It , (  x1 : x2 )' , y1 : y2 );
+  %eval gradient of template image 
+  [ Ix , Iy ] = imgradientxy( T );
+  Ix = reshape ( Ix , [ ] , 1 ) ;
+  Iy = reshape ( Iy , [ ] , 1 );
+  grad_T = [ Ix ,  Iy ];
+  % eval jacobian dw / dp at x;0
+  dw_dp = [ 1 0 ; 0 1 ];
+  % compute steepest descent images gradient ( T ) dw / dp
+  steepest = grad_T * dw_dp ;
+  disp ( size ( grad_T ) ) 
+  %%%%
+  steepest_q = zeros (  size ( steepest , 1 ), 2 );
+  A = reshape ( bases , [ ] , size ( bases , 3 ) ); % h*w X number of images
+  for x = 1 : size ( bases , 1 ) * size ( bases , 2 )
+    i_accum = [ 0 , 0 ];
+    for i = 1 : size ( bases , 3 )  
+      %Ai = reshape ( bases ( : , : , i ) )
+      % reshape Ai : nx1 , steepest nx2 
+      %y_accum = sum ( reshape ( Ai , [ ] , 1 ) .* steepest ); %1x2
+      %y_accum = sum ( reshape ( bases ( : , : ,  i )  , [ ] , 1 ) .* steepest ); %1x2
+      y_accum = sum ( A ( : , i )  .* steepest ); %1x2
+      i_accum = i_accum + A( x , i ) * y_accum;
+    end
+    steepest_q ( x , : ) = i_accum;
+  end
+  %%%%
+  H = steepest' * steepest;  % Hessian
+  u = 10;
+  v = 10;
+  p = [ 0 ; 0 ];
+  %Iterate
+  for i = 1 : max_iters
+    I_W = interp2 ( double ( It1 ) , ((  x1 : x2 ) + p(1) )' , ( y1 : y2 ) + p ( 2 ) );
+    err_image   = reshape ( I_W - T , [ ] , 1 ) ;
+    X =  double ( steepest )' * double ( err_image ) ;
+    dp = inv ( H ) * X
+    p = p - dp;
+    if norm ( dp ) ^2 < THRESHOLD
+      fprintf ( 'exiting iteration %d' , i );
+      break;
+    end
+  end
+  u = p ( 1 );
+  v = p ( 2 );
+end
+   
+
+  % input - image at time t, image at t+1, rectangle (top left, bot right coordinates)
+  % output - movement vector, [u, v] in the x- and y-directions.
--- assignments/hw6/matlab/testSylvSequence.m
+++ assignments/hw6/matlab/testSylvSequence.m
@@ -1,1 +1,29 @@
 % your code here
+function  [ ] = testSylvSequence( )
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  load ( '../data/sylvseq.mat' )   ;
+  load ( '../data/sylvbases.mat' )   ;
+  n_frames = size ( frames , 3 )  ;
+  rect = [ 102 ; 62 ; 156 ; 108 ]; % Init bounding box
+  for i = 1 : n_frames - 1
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    imshow ( frames ( : , : , i ) );
+    x = rect ( 1 );
+    y = rect ( 2 );
+    w = rect ( 3 ) - rect ( 1 );
+    h = rect ( 4 ) - rect ( 2 );
+    %rectangle ( 'Position' , rect' );
+    fuck_rect = [ x , y , w , h ];
+    rectangle ( 'Position' , [ x , y , w , h ]  );
+    [ u, v ] = LucasKanadeBasis( frames ( : , : , i     ) ,  ...
+                                                frames ( : , : , i + 1 ) ,  ...
+                                                rect                     ,  ...
+                                                bases )
+    rect
+    rect = rect + [ u ; v ; u ; v ]
+    pause ( 0.1 );
+  end
+  %[u,v] = LucasKanadeInverseCompositional(It, It1, rect)
+end
