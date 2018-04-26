9016a276b1a488d0531d0417edde72f82146cd43
jphaneuf <phaneuf.joe>
2018-04-26 08:57:37 -0400

    sylester sequenct tracks with inverse comp

--- assignments/hw6/matlab/testSylvSequence.m
+++ assignments/hw6/matlab/testSylvSequence.m
@@ -6,24 +6,34 @@ function  [ ] = testSylvSequence( )
   load ( '../data/sylvseq.mat' )   ;
   load ( '../data/sylvbases.mat' )   ;
   n_frames = size ( frames , 3 )  ;
-  rect = [ 102 ; 62 ; 156 ; 108 ]; % Init bounding box
+  rect_b = [ 102 ; 62 ; 156 ; 108 ]; % Init bounding box basis
+  rect_i = [ 102 ; 62 ; 156 ; 108 ]; % Init bounding inverse compositional
   for i = 1 : n_frames - 1
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     imshow ( frames ( : , : , i ) );
-    x = rect ( 1 );
-    y = rect ( 2 );
-    w = rect ( 3 ) - rect ( 1 );
-    h = rect ( 4 ) - rect ( 2 );
+    xb = rect_b ( 1 );
+    yb = rect_b ( 2 );
+    wb = rect_b ( 3 ) - rect_b ( 1 );
+    hb = rect_b ( 4 ) - rect_b ( 2 );
+
+    xi = rect_i ( 1 );
+    yi = rect_i ( 2 );
+    wi = rect_i ( 3 ) - rect_i ( 1 );
+    hi = rect_i ( 4 ) - rect_i ( 2 );
+
     %rectangle ( 'Position' , rect' );
-    fuck_rect = [ x , y , w , h ];
-    rectangle ( 'Position' , [ x , y , w , h ]  );
-    [ u, v ] = LucasKanadeBasis( frames ( : , : , i     ) ,  ...
+    rectangle ( 'Position' , [ xb , yb , wb , hb ] , 'EdgeColor' , 'b' , 'LineWidth' , 2 );
+    rectangle ( 'Position' , [ xi , yi , wi , hi ] ,'EdgeColor', 'r' );
+    [ ub, vb ] = LucasKanadeBasis( frames ( : , : , i     ) ,  ...
                                                 frames ( : , : , i + 1 ) ,  ...
-                                                rect                     ,  ...
+                                                rect_b                   ,  ...
                                                 bases )
-    rect
-    rect = rect + [ u ; v ; u ; v ]
+    [ ui, vi ] = LucasKanadeInverseCompositional( frames ( : , : , i     ) ,  ...
+                                                frames ( : , : , i + 1 ) ,  ...
+                                                rect_i                     )
+
+    rect_b = rect_b + [ ub ; vb ; ub ; vb ]
+    rect_i = rect_i + [ ui ; vi ; ui ; vi ]
     pause ( 0.1 );
   end
-  %[u,v] = LucasKanadeInverseCompositional(It, It1, rect)
 end
