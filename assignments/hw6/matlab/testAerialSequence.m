6113fe7ccd2ea2a1df39a98cb3353d15c3865182
jphaneuf <phaneuf.joe>
2018-04-28 11:08:19 -0400

    aeeeriiaaallls, on the ground, make those outlinessss sooo profoound

--- assignments/hw6/matlab/testAerialSequence.m
+++ assignments/hw6/matlab/testAerialSequence.m
@@ -1,1 +1,17 @@
 % your code here
+function  [ ] = testUSSequenceAffine( )
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  load ( '../data/aerialseq.mat' )   ;
+  n_frames = size ( frames , 3 )  ;
+  for i = 1 : n_frames - 1
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    image_i   = frames ( : , : , i     );
+    image_ip1 = frames ( : , : , i + 1 );
+    [ mask ] = SubtractDominantMotion( image_i , image_ip1 );
+    fused_image = imfuse ( image_i , mask );
+    imshow ( fused_image );
+    pause ( 0.01 )
+  end
+end
